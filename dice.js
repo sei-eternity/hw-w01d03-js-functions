d948c3f085f7f31b59e20e8249e8fbd8a2ed1059
Abdullah <a_t1411@hotmail.com>
2019-06-18 17:36:15 +0300

    W01D03 HW

--- dice.js
+++ dice.js
@@ -2,7 +2,7 @@ const rollDice = function(){
     let randomNum1 = 6 - Math.floor(Math.random() * Math.floor(6));
     let randomNum2 = 6 - Math.floor(Math.random() * Math.floor(6));
     let sum =  randomNum1 + randomNum2;
-    return sum;
+    return "Dice rolled are "+randomNum1+" and "+randomNum2+". Sum: "+ sum;
    
    }
    rollDice();
