92898c40461c76f2ba5c7e47209a921846cdc87d
outsiderem <reemharbi.3@gmail.com>
2019-06-18 17:22:54 +0300

    Dice Done

--- dice.js
+++ dice.js
@@ -1,10 +1,10 @@
-// rollDice(){
-const diceChoice1 = Math.floor(Math.random() * 6) + 1;
-console.log(diceChoice1);
-const diceChoice2 = Math.floor(Math.random() * 6) + 1;
-console.log(diceChoice2);
+const rollDice = function (){
+    const diceChoice1 = Math.floor(Math.random() * 6) + 1;
+    console.log(diceChoice1);
+    const diceChoice2 = Math.floor(Math.random() * 6) + 1;
+    console.log(diceChoice2);    
+    const sum = diceChoice1 + diceChoice2;
+return `Dice rolled are ${diceChoice1} and ${diceChoice2}. Sum is ${sum}`;
+}
 
-// const sum = diceChoice1 + diceChoice2;
-
-// }
-// console.log(computerChoice);
+rollDice();
