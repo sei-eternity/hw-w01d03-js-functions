27257828710caab8266a1e4671ae1dabd959558b
Ahmed AlMeshaal <ahmedmeshaal.66@gmail.com>
2019-06-19 08:24:19 +0300

    Ahmed Al Meshaal

---
+++ rps.js
@@ -0,0 +1,34 @@
+const game1 = function(userRole) {
+    var myArray = ["rock","paper","scissors"];
+
+var playGame = myArray[Math.floor(Math.random()*myArray.length)];
+
+if(playGame == userRole){
+return "The cumputer plays " + playGame + ", and the user plays " + userRole + " ,so the tie the game"; 
+}
+ else if (playGame == "rock" && userRole =="scissors")
+ {
+     return "The computer win";
+ }   
+ else if (playGame == "scissors" && userRole =="paper")
+ {
+     return "The computer win";
+ } 
+ else if (playGame == "paper" && userRole =="rock")
+ {
+     return "The computer win";
+ }
+ else if (playGame == "paper" && userRole =="scissors")
+ {
+     return "The user win";
+ }
+ else if (playGame == "rock" && userRole =="paper")
+ {
+     return "The user win";
+ }
+ else if (playGame == "scissors" && userRole =="rock")
+ {
+     return "The user win";
+ }}
+
+ game1("scissors");
