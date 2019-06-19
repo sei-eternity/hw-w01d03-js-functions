94a5b62a7c0a61d5111264cfbf3ea6517f341529
Ahmed AlMeshaal <ahmedmeshaal.66@gmail.com>
2019-06-19 08:03:58 +0300

    Ahmed Al Meshaal

--- dice.js
+++
@@ -1,9 +0,0 @@
-const rollDice1 = function(numOne, numTwo){
-    var myArray = [1,2,3,4,5,6];
-
-var numOne = myArray[Math.floor(Math.random()*myArray.length)];
-var numTwo = myArray[Math.floor(Math.random()*myArray.length)];
-    
-
-    const sum = numOne + numTwo;
-    return "Dice rolled are " + numOne + " and " + numTwo + " . Sum is " + sum ;}
